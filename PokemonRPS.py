7cc6b2a6670fb0e488ab9b077d4d6921995c221b
Shogue21 <72211190+Shogue21@users.noreply.github.com>
2020-10-07 08:25:18 -0500

    Added changes from test branch to main

--- PokemonRPS.py
+++ PokemonRPS.py
@@ -2,14 +2,15 @@ import random
 from colorama import Fore
 from colorama import Style
 
-choices = ['Fire', 'Ice', 'Grass', 'Ground','Rock']
-fireOptions = ['Fire', 'fire', 'F', 'f']
-iceOptions = ['Ice', 'ice', 'I', 'i']
-grassOptions = ['Grass', 'grass', 'G', 'g']
-groundOptions = ['Ground','ground','GR','gr']
-rockOptions = ['Rock', 'rock','R','r']
-yesOptions = ['Yes', 'yes', 'Y', 'y']
-colors = [Fore.RED, Fore.BLUE, Fore.GREEN, Fore.LIGHTCYAN_EX, Fore.LIGHTMAGENTA_EX]
+choices = ("Fire", "Grass", "Rock", "Ice", "Ground")
+fireOptions = ('Fire', 'fire', 'F', 'f')
+grassOptions = ('Grass', 'grass', 'Gra', 'gra')
+rockOptions = ('Rock', 'rock', 'R', 'r')
+iceOptions = ('Ice', 'ice', 'I', 'i')
+groundOptions = ('Ground', 'ground', 'Gro', 'gro')
+yesOptions = ('Yes', 'yes', 'Y', 'y')
+zippedOptions = list(zip(fireOptions, grassOptions, rockOptions, iceOptions, groundOptions))
+colors = [Fore.RED, Fore.GREEN, Fore.YELLOW, Fore.LIGHTBLUE_EX, Fore.LIGHTYELLOW_EX]
 playerWins = 0
 comWins = 0
 roundNum = 1
@@ -19,16 +20,18 @@ def validType(choice: str) -> bool: # Checks if the user's choice is valid.
     True
     >>> validType("Grass")
     True
-    >>> validType("Electric")
+    >>> validType("Rock")
     False
     '''
-    return choice in fireOptions or choice in iceOptions or choice in grassOptions or choice in groundOptions or choice in rockOptions
+    for item in zippedOptions:
+        if choice in item:
+            return True
 
 def userSettings(): # Function to determine the player's type choice.
     playerChoice = ''
     while not validType(playerChoice):
         playerChoice = input("Please choose from these types: ")
-        if playerChoice not in choices:
+        if not validType(playerChoice):
             print("Please choose a valid type!\n")
     return playerChoice
 
@@ -41,7 +44,12 @@ count = 0
 for item in choices:
     print(f"{colors[count]}{item}{Style.RESET_ALL}")
     count += 1
-print("\nRemember, fire beats grass and ice, ice beats grass and ground, grass beats fire and rock , and rock beats fire and ice \n")
+print(f'''\nRemember:
+{Fore.RED}Fire{Style.RESET_ALL} beats {Fore.GREEN}grass{Style.RESET_ALL} and {Fore.BLUE}ice {Style.RESET_ALL}
+{Fore.GREEN}Grass{Style.RESET_ALL} beats {Fore.YELLOW}rock{Style.RESET_ALL} and {Fore.LIGHTYELLOW_EX}ground
+{Fore.YELLOW}Rock{Style.RESET_ALL} beats {Fore.RED}fire{Style.RESET_ALL} and {Fore.BLUE}ice{Style.RESET_ALL}
+{Fore.BLUE}Ice{Style.RESET_ALL} beats {Fore.GREEN}grass{Style.RESET_ALL} and {Fore.LIGHTYELLOW_EX}ground{Style.RESET_ALL}
+{Fore.LIGHTYELLOW_EX}Ground{Style.RESET_ALL} beats {Fore.RED}fire{Style.RESET_ALL} and {Fore.YELLOW}rock{Style.RESET_ALL}!''')
 
 while True: # Main portion of RPS game
     print("")
@@ -52,89 +60,88 @@ while True: # Main portion of RPS game
     # If statements to determine the winner of the match, adds the result to score.
     if userChoice in fireOptions:
         if comChoice == 'Fire':
-            print("You both chose fire! It's a tie!")
-            playerWins +=0
-        elif comChoice == 'Ice':
-            print("The computer chose ice! You win!")
-            playerWins +=1
-        elif comChoice == 'Grass':
-            print("The computer choose grass! You win!")
-            playerWins +=1
-        elif comChoice == 'Ground':
-            print("The computer choose ground ! You lose!")
-            comWins+=1
-        elif comChoice == 'Rock':
-            print("The computer choose rock! You lose!")
-            comWins+=1
-
-    elif userChoice in iceOptions:
-        if comChoice == 'Ice':
-            print("You both chose ice! It's a tie!")
+            print(f"You both chose {Fore.RED}fire{Style.RESET_ALL}! It's a tie!")
             playerWins += 0
-        elif comChoice == 'Grass':
-            print("The computer chose grass! You lose!")
-            comWins += 1
-        elif comChoice == 'Fire':
-            print("The computer choose fire! You lose!")
-            comWins += 1
         elif comChoice == 'Ground':
-            print("The computer choose ground ! You win!")
-            playerWins+=1
+            print(f"The computer chose {Fore.LIGHTYELLOW_EX}ground{Style.RESET_ALL}! You lose!")
+            comWins += 1
         elif comChoice == 'Rock':
-            print("The computer choose rock! You win!")
-            playerWins+=1
+            print(f"The computer chose {Fore.YELLOW}rock{Style.RESET_ALL}! You lose!")
+            comWins += 1
+        elif comChoice == 'Grass':
+            print(f"The computer chose {Fore.GREEN}grass{Style.RESET_ALL}! You win!")
+            playerWins += 1
+        elif comChoice == 'Ice':
+            print(f"The computer chose {Fore.BLUE}ice{Style.RESET_ALL}! You win!")
+            playerWins += 1
 
     elif userChoice in grassOptions:
         if comChoice == 'Grass':
-            print("You both chose grass! It's a tie!")
+            print(f"You both chose {Fore.GREEN}grass{Style.RESET_ALL}! It's a tie!")
             playerWins += 0
         elif comChoice == 'Fire':
-            print("The computer chose fire! You lose!")
+            print(f"The computer chose {Fore.RED}fire{Style.RESET_ALL}! You lose!")
             comWins += 1
         elif comChoice == 'Ice':
-            print("The computer choose ice! You lose!")
+            print(f"The computer chose {Fore.BLUE}ice{Style.RESET_ALL}! You lose!")
             comWins += 1
-        elif comChoice == 'Ground':
-            print("The computer choose ground ! You win!")
-            playerWins+=1
         elif comChoice == 'Rock':
-            print("The computer choose rock! You win!")
-            playerWins+=1
+            print(f"The computer chose {Fore.YELLOW}rock{Style.RESET_ALL}! You win!")
+            playerWins += 1
+        elif comChoice == 'Ground':
+            print(f"The computer chose {Fore.LIGHTYELLOW_EX}ground{Style.RESET_ALL}! You win!")
+            playerWins += 1
+    
+    elif userChoice in rockOptions:
+        if comChoice == 'Rock':
+            print(f"You both chose {Fore.YELLOW}rock{Style.RESET_ALL}! It's a tie!")
+            playerWins += 0
+        elif comChoice == 'Grass':
+            print(f"The computer chose {Fore.GREEN}grass{Style.RESET_ALL}! You lose!")
+            comWins += 1
+        elif comChoice == "Ground":
+            print(f"The computer chose {Fore.LIGHTYELLOW_EX}ground{Style.RESET_ALL}! You lose!")
+            comWins += 1
+        elif comChoice == 'Fire':
+            print(f"The computer chose {Fore.RED}fire{Style.RESET_ALL}! You win!")
+            playerWins += 1
+        elif comChoice == 'Ice':
+            print(f"The computer chose {Fore.BLUE}ice{Style.RESET_ALL}! You win!")
+            playerWins += 1
+
+    elif userChoice in iceOptions:
+        if comChoice == 'Ice':
+            print(f"You both chose {Fore.BLUE}ice{Style.RESET_ALL}! It's a tie!")
+            playerWins += 0
+        elif comChoice == 'Fire':
+            print(f"The computer chose {Fore.RED}fire{Style.RESET_ALL}! You lose!")
+            comWins += 1
+        elif comChoice == 'Rock':
+            print(f"The computer chose {Fore.YELLOW}rock{Style.RESET_ALL}! You lose!")
+            comWins += 1
+        elif comChoice == 'Grass':
+            print(f"The computer chose {Fore.GREEN}grass{Style.RESET_ALL}! You win!")
+            playerWins += 1
+        elif comChoice == 'Ground':
+            print(f"The computer chose {Fore.LIGHTYELLOW_EX}ground{Style.RESET_ALL}! You win!")
+            playerWins += 1
 
     elif userChoice in groundOptions:
         if comChoice == 'Ground':
-            print("The computer choose ground! It's a tie!")
-            playerWins +=0
+            print(f"You both chose {Fore.LIGHTYELLOW_EX}ground{Style.RESET_ALL}! It's a tie!")
+            playerWins += 0
         elif comChoice == 'Grass':
-            print("The computer choose grass! You lose!")
-            comWins+=1
-        elif comChoice == 'Fire':
-            print("The computer choose fire! You win!")
-            playerWins+=1
+            print(f"The computer chose {Fore.GREEN}grass{Style.RESET_ALL}! You lose!")
+            comWins += 1
         elif comChoice == 'Ice':
-            print("The computer choose ice! You lose!")
-            comWins+=1
+            print(f"The computer chose {Fore.BLUE}ice{Style.RESET_ALL}! You lose!")
+            comWins += 1
+        elif comChoice == 'Fire':
+            print(f"The computer chose {Fore.RED}fire{Style.RESET_ALL}! You win!")
+            playerWins += 1
         elif comChoice == 'Rock':
-            print("The computer choose rock! You win!")
-            playerWins+=1
-
-    elif userChoice in rockOptions:
-        if comChoice == 'Rock':
-            print("The computer choose rock! It's a tie!")
-            playerWins+=0
-        elif comChoice == 'Fire':
-            print("The computer choose fire! You win!")
-            playerWins+=1
-        elif comChoice == 'Ice':
-            print("The computer choose ice! You win!")
-            playerWins+=1
-        elif comChoice == 'Ground':
-            print("The computer choose ground! You lose!")
-            comWins+=1
-        elif comChoice == 'Grass':
-            print("The computer choose grass! You lose!")
-            comWins+=1
-
+            print(f"The computer chose {Fore.YELLOW}rock{Style.RESET_ALL}! You win!")
+            playerWins += 1
     # Displays the amount of wins for the player and computer.
     print("")
     print(f"Player wins: {playerWins}")
@@ -142,7 +149,7 @@ while True: # Main portion of RPS game
     print("")
 
     # Asks the user if they want to play again, if 'y', the game loops back. Otherwise, the program ends.
-    playAgain = input("Do you want to play again? [Y/N]")
+    playAgain = input("Do you want to play again? [Y/N]: ")
     if playAgain in yesOptions:
         roundNum += 1
         print(f"Round {roundNum}")
