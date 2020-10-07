5da3659b2f8a1ab396823823fec9c72c478724d6
Seth Hogue <shogue21@basecampcodingacademy.org>
2020-10-07 10:52:34 -0500

    Added health and miss mechanics
    made main() function
    player and computer both start with 100 health and lose 20 health for each loss.
    Both the player and computer have a 1 in 5 chance to miss their attack if they win the battle.
    Game now resets and repeats if the user says they want to play again

--- PokemonRPS.py
+++ PokemonRPS.py
@@ -11,8 +11,6 @@ groundOptions = ('Ground', 'ground', 'Gro', 'gro')
 yesOptions = ('Yes', 'yes', 'Y', 'y')
 zippedOptions = list(zip(fireOptions, grassOptions, rockOptions, iceOptions, groundOptions))
 colors = [Fore.RED, Fore.GREEN, Fore.YELLOW, Fore.LIGHTBLUE_EX, Fore.LIGHTYELLOW_EX]
-playerWins = 0
-comWins = 0
 roundNum = 1
 def validType(choice: str) -> bool: # Checks if the user's choice is valid.
     ''' Returns True if type is in the choices list. Returns False otherwise.
@@ -39,121 +37,155 @@ def comSettings(): # Function to determine the computer's type choice.
     comChoice = random.choice(choices)
     return comChoice
 
-print("Here are your choices: ") # Lists the options available to the user.
-count = 0
-for item in choices:
-    print(f"{colors[count]}{item}{Style.RESET_ALL}")
-    count += 1
-# Prints the type matchups with matching colors.
-print(f'''\nRemember:
-{Fore.RED}Fire{Style.RESET_ALL} beats {Fore.GREEN}grass{Style.RESET_ALL} and {Fore.BLUE}ice {Style.RESET_ALL}
-{Fore.GREEN}Grass{Style.RESET_ALL} beats {Fore.YELLOW}rock{Style.RESET_ALL} and {Fore.LIGHTYELLOW_EX}ground
-{Fore.YELLOW}Rock{Style.RESET_ALL} beats {Fore.RED}fire{Style.RESET_ALL} and {Fore.BLUE}ice{Style.RESET_ALL}
-{Fore.BLUE}Ice{Style.RESET_ALL} beats {Fore.GREEN}grass{Style.RESET_ALL} and {Fore.LIGHTYELLOW_EX}ground{Style.RESET_ALL}
-{Fore.LIGHTYELLOW_EX}Ground{Style.RESET_ALL} beats {Fore.RED}fire{Style.RESET_ALL} and {Fore.YELLOW}rock{Style.RESET_ALL}!''')
+# Placeholder variables until health mechanism is finished.
+def playerDamage():
+    playerDamage = 20
+    missAttack = random.randint(1, 5)
+    if missAttack == 1:
+        playerDamage = 0
+        print("\nYour attack missed!")
+    return playerDamage
 
-while True: # Main portion of RPS game
-    print("")
-    userChoice = userSettings() # Runs the previously defined function
-    comChoice = comSettings() # Runs the previously defined function
-    print("")
+def comDamage():
+    comDamage = 20
+    missAttack = random.randint(1, 5)
+    if missAttack == 1:
+        comDamage = 0
+        print("\nThe computer's attack missed!")
+    return comDamage
 
-    # If statements to determine the winner of the match, adds the result to score.
-    if userChoice in fireOptions:
-        if comChoice == 'Fire':
-            print(f"You both chose {Fore.RED}fire{Style.RESET_ALL}! It's a tie!")
-            playerWins += 0
-        elif comChoice == 'Ground':
-            print(f"The computer chose {Fore.LIGHTYELLOW_EX}ground{Style.RESET_ALL}! You lose!")
-            comWins += 1
-        elif comChoice == 'Rock':
-            print(f"The computer chose {Fore.YELLOW}rock{Style.RESET_ALL}! You lose!")
-            comWins += 1
-        elif comChoice == 'Grass':
-            print(f"The computer chose {Fore.GREEN}grass{Style.RESET_ALL}! You win!")
-            playerWins += 1
-        elif comChoice == 'Ice':
-            print(f"The computer chose {Fore.BLUE}ice{Style.RESET_ALL}! You win!")
-            playerWins += 1
+   
+def main():
+    playerHealth = 100
+    comHealth = 100
+    print("Here are your choices: ") # Lists the options available to the user.
+    count = 0
+    for item in choices:
+        print(f"{colors[count]}{item}{Style.RESET_ALL}")
+        count += 1
+    # Prints the type matchups with matching colors.
+    print(f'''\nRemember:
+    {Fore.RED}Fire{Style.RESET_ALL} beats {Fore.GREEN}grass{Style.RESET_ALL} and {Fore.BLUE}ice {Style.RESET_ALL}
+    {Fore.GREEN}Grass{Style.RESET_ALL} beats {Fore.YELLOW}rock{Style.RESET_ALL} and {Fore.LIGHTYELLOW_EX}ground
+    {Fore.YELLOW}Rock{Style.RESET_ALL} beats {Fore.RED}fire{Style.RESET_ALL} and {Fore.BLUE}ice{Style.RESET_ALL}
+    {Fore.BLUE}Ice{Style.RESET_ALL} beats {Fore.GREEN}grass{Style.RESET_ALL} and {Fore.LIGHTYELLOW_EX}ground{Style.RESET_ALL}
+    {Fore.LIGHTYELLOW_EX}Ground{Style.RESET_ALL} beats {Fore.RED}fire{Style.RESET_ALL} and {Fore.YELLOW}rock{Style.RESET_ALL}!''')
+    while playerHealth > 0 and comHealth > 0: # Main portion of RPS game
+        print("")
+        userChoice = userSettings() # Runs the previously defined function
+        comChoice = comSettings() # Runs the previously defined function
+        print("")
 
-    elif userChoice in grassOptions:
-        if comChoice == 'Grass':
-            print(f"You both chose {Fore.GREEN}grass{Style.RESET_ALL}! It's a tie!")
-            playerWins += 0
-        elif comChoice == 'Fire':
-            print(f"The computer chose {Fore.RED}fire{Style.RESET_ALL}! You lose!")
-            comWins += 1
-        elif comChoice == 'Ice':
-            print(f"The computer chose {Fore.BLUE}ice{Style.RESET_ALL}! You lose!")
-            comWins += 1
-        elif comChoice == 'Rock':
-            print(f"The computer chose {Fore.YELLOW}rock{Style.RESET_ALL}! You win!")
-            playerWins += 1
-        elif comChoice == 'Ground':
-            print(f"The computer chose {Fore.LIGHTYELLOW_EX}ground{Style.RESET_ALL}! You win!")
-            playerWins += 1
-    
-    elif userChoice in rockOptions:
-        if comChoice == 'Rock':
-            print(f"You both chose {Fore.YELLOW}rock{Style.RESET_ALL}! It's a tie!")
-            playerWins += 0
-        elif comChoice == 'Grass':
-            print(f"The computer chose {Fore.GREEN}grass{Style.RESET_ALL}! You lose!")
-            comWins += 1
-        elif comChoice == "Ground":
-            print(f"The computer chose {Fore.LIGHTYELLOW_EX}ground{Style.RESET_ALL}! You lose!")
-            comWins += 1
-        elif comChoice == 'Fire':
-            print(f"The computer chose {Fore.RED}fire{Style.RESET_ALL}! You win!")
-            playerWins += 1
-        elif comChoice == 'Ice':
-            print(f"The computer chose {Fore.BLUE}ice{Style.RESET_ALL}! You win!")
-            playerWins += 1
+        # If statements to determine the winner of the match, adds the result to score.
+        if userChoice in fireOptions:
+            if comChoice == 'Fire':
+                print(f"You both chose {Fore.RED}fire{Style.RESET_ALL}! It's a tie!")
+                playerHealth -= 0
+                comHealth -= 0
+            elif comChoice == 'Ground':
+                print(f"The computer chose {Fore.LIGHTYELLOW_EX}ground{Style.RESET_ALL}! You lose!")
+                playerHealth -= comDamage()
+            elif comChoice == 'Rock':
+                print(f"The computer chose {Fore.YELLOW}rock{Style.RESET_ALL}! You lose!")
+                playerHealth -= comDamage()
+            elif comChoice == 'Grass':
+                print(f"The computer chose {Fore.GREEN}grass{Style.RESET_ALL}! You win!")
+                comHealth -= playerDamage()
+            elif comChoice == 'Ice':
+                print(f"The computer chose {Fore.BLUE}ice{Style.RESET_ALL}! You win!")
+                comHealth -= playerDamage()
 
-    elif userChoice in iceOptions:
-        if comChoice == 'Ice':
-            print(f"You both chose {Fore.BLUE}ice{Style.RESET_ALL}! It's a tie!")
-            playerWins += 0
-        elif comChoice == 'Fire':
-            print(f"The computer chose {Fore.RED}fire{Style.RESET_ALL}! You lose!")
-            comWins += 1
-        elif comChoice == 'Rock':
-            print(f"The computer chose {Fore.YELLOW}rock{Style.RESET_ALL}! You lose!")
-            comWins += 1
-        elif comChoice == 'Grass':
-            print(f"The computer chose {Fore.GREEN}grass{Style.RESET_ALL}! You win!")
-            playerWins += 1
-        elif comChoice == 'Ground':
-            print(f"The computer chose {Fore.LIGHTYELLOW_EX}ground{Style.RESET_ALL}! You win!")
-            playerWins += 1
+        elif userChoice in grassOptions:
+            if comChoice == 'Grass':
+                print(f"You both chose {Fore.GREEN}grass{Style.RESET_ALL}! It's a tie!")
+                playerHealth -= 0
+                comHealth -= 0
+            elif comChoice == 'Fire':
+                print(f"The computer chose {Fore.RED}fire{Style.RESET_ALL}! You lose!")
+                playerHealth -= comDamage()
+            elif comChoice == 'Ice':
+                print(f"The computer chose {Fore.BLUE}ice{Style.RESET_ALL}! You lose!")
+                playerHealth -= comDamage()
+            elif comChoice == 'Rock':
+                print(f"The computer chose {Fore.YELLOW}rock{Style.RESET_ALL}! You win!")
+                comHealth -= playerDamage()
+            elif comChoice == 'Ground':
+                print(f"The computer chose {Fore.LIGHTYELLOW_EX}ground{Style.RESET_ALL}! You win!")
+                comHealth -= playerDamage()
+        
+        elif userChoice in rockOptions:
+            if comChoice == 'Rock':
+                print(f"You both chose {Fore.YELLOW}rock{Style.RESET_ALL}! It's a tie!")
+                playerHealth -= 0
+                comHealth -= 0
+            elif comChoice == 'Grass':
+                print(f"The computer chose {Fore.GREEN}grass{Style.RESET_ALL}! You lose!")
+                playerHealth -= comDamage()
+            elif comChoice == "Ground":
+                print(f"The computer chose {Fore.LIGHTYELLOW_EX}ground{Style.RESET_ALL}! You lose!")
+                playerHealth -= comDamage()
+            elif comChoice == 'Fire':
+                print(f"The computer chose {Fore.RED}fire{Style.RESET_ALL}! You win!")
+                comHealth -= playerDamage()
+            elif comChoice == 'Ice':
+                print(f"The computer chose {Fore.BLUE}ice{Style.RESET_ALL}! You win!")
+                comHealth -= playerDamage()
 
-    elif userChoice in groundOptions:
-        if comChoice == 'Ground':
-            print(f"You both chose {Fore.LIGHTYELLOW_EX}ground{Style.RESET_ALL}! It's a tie!")
-            playerWins += 0
-        elif comChoice == 'Grass':
-            print(f"The computer chose {Fore.GREEN}grass{Style.RESET_ALL}! You lose!")
-            comWins += 1
-        elif comChoice == 'Ice':
-            print(f"The computer chose {Fore.BLUE}ice{Style.RESET_ALL}! You lose!")
-            comWins += 1
-        elif comChoice == 'Fire':
-            print(f"The computer chose {Fore.RED}fire{Style.RESET_ALL}! You win!")
-            playerWins += 1
-        elif comChoice == 'Rock':
-            print(f"The computer chose {Fore.YELLOW}rock{Style.RESET_ALL}! You win!")
-            playerWins += 1
-    # Displays the amount of wins for the player and computer.
-    print("")
-    print(f"Player wins: {playerWins}")
-    print(f"Computer wins: {comWins}")
-    print("")
+        elif userChoice in iceOptions:
+            if comChoice == 'Ice':
+                print(f"You both chose {Fore.BLUE}ice{Style.RESET_ALL}! It's a tie!")
+                playerHealth -= 0
+                comHealth -= 0
+            elif comChoice == 'Fire':
+                print(f"The computer chose {Fore.RED}fire{Style.RESET_ALL}! You lose!")
+                playerHealth -= comDamage()
+            elif comChoice == 'Rock':
+                print(f"The computer chose {Fore.YELLOW}rock{Style.RESET_ALL}! You lose!")
+                playerHealth -= comDamage()
+            elif comChoice == 'Grass':
+                print(f"The computer chose {Fore.GREEN}grass{Style.RESET_ALL}! You win!")
+                comHealth -= playerDamage()
+            elif comChoice == 'Ground':
+                print(f"The computer chose {Fore.LIGHTYELLOW_EX}ground{Style.RESET_ALL}! You win!")
+                comHealth -= playerDamage()
 
-    # Asks the user if they want to play again, if 'y', the game loops back. Otherwise, the program ends.
+        elif userChoice in groundOptions:
+            if comChoice == 'Ground':
+                print(f"You both chose {Fore.LIGHTYELLOW_EX}ground{Style.RESET_ALL}! It's a tie!")
+                playerHealth -= 0
+                comHealth -= 0
+            elif comChoice == 'Grass':
+                print(f"The computer chose {Fore.GREEN}grass{Style.RESET_ALL}! You lose!")
+                playerHealth -= comDamage()
+            elif comChoice == 'Ice':
+                print(f"The computer chose {Fore.BLUE}ice{Style.RESET_ALL}! You lose!")
+                playerHealth -= comDamage()
+            elif comChoice == 'Fire':
+                print(f"The computer chose {Fore.RED}fire{Style.RESET_ALL}! You win!")
+                comHealth -= playerDamage()
+            elif comChoice == 'Rock':
+                print(f"The computer chose {Fore.YELLOW}rock{Style.RESET_ALL}! You win!")
+                comHealth -= playerDamage()
+        # Displays the amount of wins for the player and computer.
+        print("")
+        print(f"Player health: {playerHealth}")
+        print(f"Computer health: {comHealth}")
+        print("")
+
+        if playerHealth == 0:
+            print("You were defeated by the computer!")
+        elif comHealth == 0:
+            print("You defeated the computer!")
+
+main()
+
+# Asks the user if they want to play again, if 'y', the game loops back. Otherwise, the program ends.
+while True:
     playAgain = input("Do you want to play again? [Y/N]: ")
     if playAgain in yesOptions:
         roundNum += 1
         print(f"Round {roundNum}")
+        main()
     else:
         print("I hope you had fun!")
         break
